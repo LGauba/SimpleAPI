44a7a7b162280c683aaa87353119b5422f35bde4
Gauba, Lalit <lgauba@dxc.com>
2020-07-06 17:36:44 +0530

    FirstDeploy

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "playbookLalit" };
+            return new string[] { "dotnet", "FirstDeploy" };
         }
 
         // GET api/values/5
